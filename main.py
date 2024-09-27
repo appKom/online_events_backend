53da8a94d7855ce7bc0b71903206e366b0d0a6ae
fredrir <fhansteen@gmail.com>
2024-09-27 13:10:58 +0200

    set up som interest groups paths

--- main.py
+++ main.py
@@ -6,3 +6,28 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+class User:
+    id: str
+    name: str
+    InterestGroups: list['InterestGroup']
+    authToken: str
+
+class InterestGroup:
+    id: str
+    name: str
+    description: str
+    members: list['User', str]
+    
+    
+
+
+@app.get("/interest-groups")
+async def get_interest_groups():
+    return {"message": "Interest Groups"}
+
+
+@app.put("interest-groups/{interest_group_id}")
+async def update_interest_group(interest_group_id: str):
+    return {"message": "Interest Group Updated"}
