c40e1d4394c3d4f116f9a1a409755184c3ba0cbc
fredrir <fhansteen@gmail.com>
2024-09-27 13:21:22 +0200

    setup API routes for user and interest groups

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+import json
+from pydantic import BaseModel
+from urllib.request import urlopen
 
 app = FastAPI()
 
@@ -12,7 +15,6 @@ class User:
     id: str
     name: str
     InterestGroups: list['InterestGroup']
-    authToken: str
 
 class InterestGroup:
     id: str
@@ -20,7 +22,46 @@ class InterestGroup:
     description: str
     members: list['User', str]
     
+
+def auhtenticate_user(auth_token: str):
+    response = urlopen(("https://old.online.ntnu.no/api/v1/profile/"))
+    data_json = json.loads(response.read())
     
+    if data_json["id"] is None:
+        return None
+    
+    
+    
+    user_id = data_json['id']
+    user = get_user(user_id)
+    
+    if user is None:
+        create_user(user)
+    
+    
+    return User(data_json['id'], data_json['name'], data_json['InterestGroups'], auth_token)
+    
+    
+
+
+@app.get("/user/{user_id}")
+async def get_user(user_id: str):
+    
+    response = urlopen(("TODO: URL to get user"))
+    
+    if response.status != 200:
+        return {"message": "User not found"}
+    
+    data_json = json.loads(response.read())
+    
+    
+    return User(data_json['id'], data_json['name'], data_json['InterestGroups'])
+    
+@app.post("/user")
+async def create_user(user: User):
+    #TODO 
+    
+    return {"message": "User Created"}
 
 
 @app.get("/interest-groups")
@@ -29,5 +70,10 @@ async def get_interest_groups():
 
 
 @app.put("interest-groups/{interest_group_id}")
-async def update_interest_group(interest_group_id: str):
+async def update_interest_group(interest_group_id: str, auth_token: str):
+    
+    
+    user = auhtenticate_user(auth_token)
+    
+
     return {"message": "Interest Group Updated"}
